ceca1fa5889ad45095534a2cf7b7c487d973c507
dac-phuong <nguyendacphuong14@gmal.com>
2022-09-07 03:16:27 -0700

    up

--- acsset/javascript/main.js
+++ acsset/javascript/main.js
@@ -15,26 +15,7 @@ foodBtns.forEach((btn) => {
 		})
 	})
 })
- 
 
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" activity", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " activity"; 
-}
 const wrapper = document.querySelector('.section-logos')
 let isMouseDown = false
 let startX, scrollLeft
@@ -68,4 +49,8 @@ function none (){
 function block (){
 	add = document.querySelector(".menu-mobile")
 	add.style.display = "block"
+}
+function add (){
+	block = document.querySelector(".search-input")
+	block.style.display = "block"
 }
